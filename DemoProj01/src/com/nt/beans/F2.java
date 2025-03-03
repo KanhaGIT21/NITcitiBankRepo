8e3c3a647e19d8f72f60e2c38593886f99639f91
TL <TL@gmail.com>
2025-03-03 13:30:43 +0530

    stash commit

--- DemoProj01/src/com/nt/beans/F2.java
+++ DemoProj01/src/com/nt/beans/F2.java
@@ -5,7 +5,18 @@ public void y() {
 	System.out.println("F2.y()");
 }
 
+
 public void m4() {
 	System.out.println("F2.m4()");
 }
+
+public void m2() {
+	
+}
+
+public void m3() {
+	System.out.println("F2.m3()");
+}
+
+
 }
