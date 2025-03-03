83064fe9355fb1d32d9063501fcf1401bec67701
TL <TL@gmail.com>
2025-03-03 12:30:34 +0530

    some change in utilty.java

--- DemoProj01/src/com/nt/beans/HelloWorld.java
+++ DemoProj01/src/com/nt/beans/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
 	public void m1() {
 		System.out.println("m1");
 //>>>>>>> branch 'master' of https://github.com/KanhaGIT21/NITcitiBankRepo.git
+		System.out.println("change");
 	}
 }
